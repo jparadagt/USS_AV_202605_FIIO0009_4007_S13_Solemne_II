6ed85b38f0ede88d0e5ccd107b19ba208a0c7ee1
Jonathan Parada G. <j.paradagt@gmail.com>
2026-05-22 23:19:52 -0400

    Se implementa creacion de tareas

--- frontend/src/app/components/task-list/task-list.component.ts
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { Task } from '../../models/task';
 import { TASKS_MOCK } from '../../mock/tasks.mock';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskFormComponent } from '../task-form/task-form.component';
 
 @Component({
-  selector: 'app-task-list',
-  standalone: true,
-  imports: [CommonModule, TaskItemComponent],
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+	selector: 'app-task-list',
+	standalone: true,
+	imports: [CommonModule, TaskItemComponent, TaskFormComponent],
+	templateUrl: './task-list.component.html',
+	styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-
-  tasks: Task[] = TASKS_MOCK;
-
+	tasks: Task[] = TASKS_MOCK;
+	
+	addTask(task: Task): void {
+		this.tasks.unshift(task);
+	}
 }
