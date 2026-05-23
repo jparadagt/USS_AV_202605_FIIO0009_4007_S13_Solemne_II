3c67b175ecaf5aff0cfd369849f5c16f141282b4
Jonathan Parada G. <j.paradagt@gmail.com>
2026-05-23 00:46:13 -0400

    Se crea orquestacion docker-compose.yml

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const taskRoutes = require('./routes/task.routes');
 
@@ -10,7 +12,18 @@ app.use(express.json());
 
 app.use('/tasks', taskRoutes);
 
-const PORT = 3000;
+const publicPath = path.join(__dirname, 'public');
+const indexPath = path.join(publicPath, 'index.html');
+
+app.use(express.static(publicPath));
+
+if (fs.existsSync(indexPath)) {
+  app.get(/.*/, (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
