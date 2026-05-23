87b9aafebb8280729e2a9aefe8aa347f4b91c736
Jonathan Parada G. <j.paradagt@gmail.com>
2026-05-23 00:10:34 -0400

    Se agrega creacion de tarea

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -34,4 +34,59 @@ router.get('/', async (req, res) => {
 
 });
 
+router.post('/', async (req, res) => {
+
+  try {
+
+    const {
+      title,
+      description,
+      status,
+      priority,
+      createdAt,
+      dueDate
+    } = req.body;
+
+    const result = await pool.query(`
+      INSERT INTO tasks (
+        title,
+        description,
+        status,
+        priority,
+        created_at,
+        due_date
+      )
+      VALUES ($1,$2,$3,$4,$5,$6)
+      RETURNING
+        id,
+        title,
+        description,
+        status,
+        priority,
+        created_at AS "createdAt",
+        due_date AS "dueDate"
+    `,
+    [
+      title,
+      description,
+      status,
+      priority,
+      createdAt,
+      dueDate
+    ]);
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      error: 'Database error'
+    });
+
+  }
+
+});
+
 module.exports = router;
